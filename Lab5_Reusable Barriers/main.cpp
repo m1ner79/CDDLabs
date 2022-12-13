3ea0cee3b022cd18749bb2b1fd608e273869059b
michal <michal.d.gornicki@gmail.com>
2022-12-13 10:50:20 +0000

    added comments for doxygen

--- Lab5_Reusable Barriers/main.cpp
+++ Lab5_Reusable Barriers/main.cpp
@@ -1,4 +1,4 @@
-/*This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
+/*This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 */
 /**
@@ -13,47 +13,48 @@ To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-
  * open terminal in directory and run "make ALL"
  * \section sec-use Usage
  * type "./myFile" to run it
-*/
+ */
 #include "ReusableBarrier.h"
 #include <iostream>
 #include <thread>
 #include <vector>
 
-
 static const int num_threads = 100;
-int sharedVariable=0;
-
+int sharedVariable = 0;
 
 /*! \fn barrierTask
     \brief An example of using a reusable barrier
 */
 /*! displays a message that is split in to 2 sections to show how a rendezvous works*/
-void barrierTask(std::shared_ptr<ReusableBarrier> theBarrier, int numLoops){
+void barrierTask(std::shared_ptr<ReusableBarrier> theBarrier, int numLoops)
+{
 
-  for(int i=0;i<numLoops;++i){
-    //Do first bit of task
-    std::cout << "A"<< i;
-    //Barrier
+  for (int i = 0; i < numLoops; ++i)
+  {
+    // Do first bit of task
+    std::cout << "A" << i;
+    // Barrier
     theBarrier->taskA();
-    //Do second half of task
-    std::cout<< "B" << i;
+    // Do second half of task
+    std::cout << "B" << i;
     theBarrier->taskB();
   }
-  
-
 }
 
-int main(void){
+int main(void)
+{
   std::vector<std::thread> vt(num_threads);
-  std::shared_ptr<ReusableBarrier> aBarrier( new ReusableBarrier(num_threads));
+  std::shared_ptr<ReusableBarrier> aBarrier(new ReusableBarrier(num_threads));
   /**< Launch the threads  */
-  int i=0;
-  for(std::thread& t: vt){
-    t=std::thread(barrierTask,aBarrier,10);
+  int i = 0;
+  for (std::thread &t : vt)
+  {
+    t = std::thread(barrierTask, aBarrier, 10);
   }
   /**< Join the threads with the main thread */
-  for (auto& v :vt){
-      v.join();
+  for (auto &v : vt)
+  {
+    v.join();
   }
   return 0;
 }
