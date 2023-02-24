22a27982eaa8b6e1e36ce61944fcb221b38e6b5f
michal <michal.d.gornicki@gmail.com>
2023-02-24 23:59:45 +0000

    updated constructor o prevent race condition

--- Lab5_Reusable Barriers/ReusableBarrier.cpp
+++ Lab5_Reusable Barriers/ReusableBarrier.cpp
@@ -10,22 +10,23 @@ To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-
 #include <iostream>
 /**Deconstructor*/
 ReusableBarrier::~ReusableBarrier() {}
-/**Constructor that initializes the number of threads*/
+/**In this particular constructor, the countMutex variable is being initialized with a value of 1, 
+ * which is the initial value of the binary semaphore used to synchronize access to the count variable. 
+ * The numThreads variable is being initialized with the value of the Threads parameter passed to the constructor. 
+ * The count variable is being initialized with a value of 0, indicating that no threads have yet reached the barrier.*/
 ReusableBarrier::ReusableBarrier(int Threads)
-{
-    count = 0;
-    numThreads = Threads;
-}
+: countMutex(1), numThreads(Threads), count(0){}
+
 /**Method that waits for all threads to reach the first phase of the barrier*/
 void ReusableBarrier::taskA()
 {
-    /**Lock the mutex*/
-    mutex.Wait();
+    /**Lock mutex*/
+    countMutex.Wait();
 
     /**Increment the count of threads that have reached the barrier*/
     count += 1;
 
-    mutex.Signal();
+    countMutex.Signal();
     /**If this is the last thread to reach the barrier,unblock the other 
      * threads by signaling the innerGate condition variable*/
     if (count == numThreads)
@@ -47,12 +48,12 @@ void ReusableBarrier::taskA()
 void ReusableBarrier::taskB()
 {
     /**Lock the mutex*/
-    mutex.Wait();
+    countMutex.Wait();
 
     /**Decrement the count of threads that have reached the barrier*/
     count -= 1;
 
-    mutex.Signal();
+    countMutex.Signal();
     /**If this is the last thread to leave the barrier,
      * unblock the other threads by signaling the outerGate condition variable*/
     if (count == 0)
